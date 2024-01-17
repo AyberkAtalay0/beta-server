96d48aea9e88df732b47cfb7c4d38803f8dfbda0
Ayberk Atalay <105879357+AyberkAtalay0@users.noreply.github.com>
2024-01-17 15:32:39 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+import json, numpy as np, cv2
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def index():
+    return "Accessed to index."
+
+# LETTER
+@app.route("/letter", methods=["GET"])
+def letter():
+    return "Accessed to letter audio recognizer."
+
+@app.route("/letter", methods=["POST"])
+def recognize_letters_by_audio():
+    # data = json.loads(request.data)
+
+    return jsonify(rd.choice([True, False]))
+
+if __name__ == "__main__":
+    app.run_server(debug=False, port=8547)
